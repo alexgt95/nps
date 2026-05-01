cebee472be03ca1beb900ffd5d1ea5a9ae54851a
alexgt95 <40178272+alexgt95@users.noreply.github.com>
2026-05-01 04:50:03 +0000

    W02 Prove: NPS 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,22 @@
-import { getParkData } from "./parkService.mjs";
-
+import { getParkData, parkInfoLinks } from "./parkService.mjs";
+import setHeaderFooter from "./setHeaderFooter.mjs";
+import { mediaCardTemplate } from "./templates.mjs";
 const parkData = getParkData();
 
-//SEE COMMENTS BELOW TO EVALUATE STUDENT'S UNDERSTANDING
-function parkInfoTemplate(info) { //So we can target the children of hte hero-banner class
-  return `<a href="/" class="hero-banner__title">${info.name}</a>
-  <p class="hero-banner__subtitle">
-    <span>${info.designation}</span>
-    <span>${info.states}</span>
-  </p>`;
+function setParkIntro(data) {
+  const introEl = document.querySelector(".intro");
+  introEl.innerHTML = `<h1>${parkData.fullName}</h1>
+  <p>${parkData.description}</p>`;
 }
 
-// insert data into disclaimer section
-const disclaimer = document.querySelector(".disclaimer > a");
-disclaimer.href = parkData.url; //Surgically replacing the "parkUrl" placeholder for th actual URL from the Yelowstone object
-disclaimer.innerHTML = parkData.fullName; //Same as above buyt for this one
-// update the title of the site. Notice that we can select things in the head just like in the body with querySelector
-document.querySelector("head > title").textContent = parkData.fullName; //It all makes sense now, right?
-// set the banner image
-document.querySelector(".hero-banner > img").src = parkData.images[0].url; //first of the images from the object
-// use the template function above to set the rest of the park specific info in the header
-document.querySelector(".hero-banner__content").innerHTML =
-  parkInfoTemplate(parkData);
+function setParkInfoLinks(data) {
+  const infoEl = document.querySelector(".info");
+  // we have multiple links to build...so we map to transform the array of objects into an array of HTML strings.
+  const html = data.map(mediaCardTemplate);
+  // join the array of strings into one string and insert it into the section
+  infoEl.insertAdjacentHTML("afterbegin", html.join(""));
+}
 
-//I could have updated them individually as consts which would have been more verbose and less flexible
+setHeaderFooter(parkData);
+setParkIntro(parkData);
+setParkInfoLinks(parkInfoLinks);
